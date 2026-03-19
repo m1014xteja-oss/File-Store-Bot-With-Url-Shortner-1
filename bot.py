001a818fb41dfcf3b15408834d63ef6d2b77113e
m1014xteja-oss <m1014xteja@gmail.com>
2026-03-19 06:44:54 +0530

    Remove URLs for support group and YouTube channel
    Updated support group and YouTube channel URLs to be empty.

--- bot.py
+++ bot.py
@@ -84,8 +84,8 @@ async def start(bot: Client, cmd: Message):
                         InlineKeyboardButton("Close 🚪", callback_data="closeMessage")
                     ],
                     [
-                        InlineKeyboardButton("Support Group", url="https://t.me/VJ_Bot_Disscussion"),
-                        InlineKeyboardButton("YouTube Channel", url="https://youtube.com/@Tech_VJ")
+                        InlineKeyboardButton("Support Group", url=""),
+                        InlineKeyboardButton("YouTube Channel", url="")
                     ]
                 ]
             )
@@ -126,7 +126,7 @@ async def main(bot: Client, message: Message):
                 return
 
         if message.from_user.id in Config.BANNED_USERS:
-            await message.reply_text("Sorry, You are banned!\n\nContact [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/VJ_Bot_Disscussion)",
+            await message.reply_text("Sorry, You are banned!\n\nContact [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥]()",
                                      disable_web_page_preview=True)
             return
 
